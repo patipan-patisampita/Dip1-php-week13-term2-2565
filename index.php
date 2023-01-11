57954d3fe7cb7b75bb00532f42def586b9696d87
patipan-patisampita <patipan.patisampita@gmail.com>
2023-01-11 16:29:24 +0700

    update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
-  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 
 <body>
