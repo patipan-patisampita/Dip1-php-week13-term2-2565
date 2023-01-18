40c386fdeeb56c83fae451512b5d5e1018449d54
patipan-patisampita <patipan.patisampita@gmail.com>
2023-01-18 17:00:58 +0700

    update

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php include_once("includes/header.php"); ?>
+<?php include_once("includes/navbar.php"); ?>
+
+<div class="container my-3">
+    <div class="card shadow">
+
+        <div class="card-header bg-success">
+            ระบบลงทะเบียนสมัครสมาชิก
+        </div>
+
+        <div class="card-body">
+
+            <form action="" method="post">
+                <div class="row">
+                    <div class="col-md-6">
+
+                    </div>
+                    <div class="col-md-6">
+
+                    </div>
+                </div>
+            </form>
+            
+        </div>
+    </div>
+</div>
+
+<?php include_once("includes/footer.php"); ?>
